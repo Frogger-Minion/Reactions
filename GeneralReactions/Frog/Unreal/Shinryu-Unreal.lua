d3ae1079ca8a7d7bdd49e0427d10ec5cc63c40ff
Frogger-Minion <getoffmyboardreee@gmail.com>
2026-05-04 19:36:40 +1000

    Add files via upload

---
+++ GeneralReactions/Frog/Unreal/Shinryu-Unreal.lua
@@ -0,0 +1,353 @@
+local tbl = 
+{
+	
+	{
+		data = 
+		{
+			actions = 
+			{
+			},
+			conditions = 
+			{
+			},
+			name = "-- Unreals --",
+			uuid = "c6866b7a-733d-d356-8990-972ba038b04e",
+			version = 2,
+		},
+		inheritedIndex = 1,
+	},
+	
+	{
+		data = 
+		{
+			actions = 
+			{
+				
+				{
+					data = 
+					{
+						aType = "Misc",
+						conditions = 
+						{
+							
+							{
+								"b7d9f8d2-e9fc-82e6-96c4-1492ce99c8f7",
+								true,
+							},
+							
+							{
+								"fe40f74a-a185-10a7-9c8d-8225f521d937",
+								true,
+							},
+						},
+						gVar = "ACR_TensorMagnum3_CD",
+						setTarget = true,
+						targetType = "Detection Target",
+						uuid = "d85b1d5c-e635-8752-a267-5e099bbcdaf8",
+						version = 2.1,
+					},
+				},
+				
+				{
+					data = 
+					{
+						aType = "Misc",
+						conditions = 
+						{
+							
+							{
+								"b7d9f8d2-e9fc-82e6-96c4-1492ce99c8f7",
+								true,
+							},
+							
+							{
+								"fe40f74a-a185-10a7-9c8d-8225f521d937",
+								false,
+							},
+							
+							{
+								"c93751c3-e2d6-04e7-bc97-2e268f77f7ed",
+								true,
+							},
+						},
+						gVar = "ACR_TensorMagnum3_CD",
+						setTarget = true,
+						targetType = "Detection Target",
+						uuid = "4982be08-c993-4c16-9fca-855f3a436130",
+						version = 2.1,
+					},
+				},
+			},
+			conditions = 
+			{
+				
+				{
+					data = 
+					{
+						category = "Self",
+						conditionType = 8,
+						localmapid = 1372,
+						uuid = "b7d9f8d2-e9fc-82e6-96c4-1492ce99c8f7",
+						version = 3,
+					},
+				},
+				
+				{
+					data = 
+					{
+						category = "Filter",
+						filterTargetType = "ContentID",
+						name = "tail",
+						partyTargetContentID = 5789,
+						uuid = "fe40f74a-a185-10a7-9c8d-8225f521d937",
+						version = 3,
+					},
+				},
+				
+				{
+					data = 
+					{
+						category = "Filter",
+						filterTargetType = "ContentID",
+						name = "heart",
+						partyTargetContentID = 6271,
+						uuid = "c93751c3-e2d6-04e7-bc97-2e268f77f7ed",
+						version = 3,
+					},
+				},
+			},
+			name = "Shinryu Unreal",
+			uuid = "2cbeeb72-d8bf-2cbb-b890-8814507ee6ba",
+			version = 2,
+		},
+	},
+	
+	{
+		data = 
+		{
+			actions = 
+			{
+				
+				{
+					data = 
+					{
+						aType = "ACR",
+						conditions = 
+						{
+							
+							{
+								"217ed8f9-6d6d-d200-9e59-ea5bbdc20819",
+								true,
+							},
+							
+							{
+								"ed3bc3cd-1060-9002-bf69-db0f334c79d5",
+								true,
+							},
+							
+							{
+								"c801f14c-2655-bdf2-9e27-d57c366eec5f",
+								true,
+							},
+							
+							{
+								"56dd8ef9-661f-7df1-8ceb-77bfd4305f24",
+								true,
+							},
+						},
+						gVar = "ACR_TensorMagnum3_Hotbar_ArmsLength",
+						name = "Arms Tidal",
+						uuid = "09e21802-f4b9-51c3-944b-d402c1220b10",
+						variableTogglesType = 2,
+						version = 2.1,
+					},
+				},
+				
+				{
+					data = 
+					{
+						aType = "ACR",
+						conditions = 
+						{
+							
+							{
+								"217ed8f9-6d6d-d200-9e59-ea5bbdc20819",
+								true,
+							},
+							
+							{
+								"ed3bc3cd-1060-9002-bf69-db0f334c79d5",
+								true,
+							},
+							
+							{
+								"c801f14c-2655-bdf2-9e27-d57c366eec5f",
+								true,
+							},
+							
+							{
+								"56dd8ef9-661f-7df1-8ceb-77bfd4305f24",
+								true,
+							},
+						},
+						gVar = "ACR_RikuRDM3_Hotbar_Surecast",
+						name = "SureC Tidal",
+						uuid = "24fa914f-3dea-4350-9498-b5c42257c0f2",
+						variableTogglesType = 2,
+						version = 2.1,
+					},
+					inheritedIndex = 2,
+				},
+				
+				{
+					data = 
+					{
+						aType = "ACR",
+						conditions = 
+						{
+							
+							{
+								"217ed8f9-6d6d-d200-9e59-ea5bbdc20819",
+								true,
+							},
+							
+							{
+								"ed3bc3cd-1060-9002-bf69-db0f334c79d5",
+								true,
+							},
+							
+							{
+								"9c1a1301-1d97-0861-b6ab-464748ee0f71",
+								true,
+							},
+							
+							{
+								"56dd8ef9-661f-7df1-8ceb-77bfd4305f24",
+								true,
+							},
+						},
+						gVar = "ACR_TensorMagnum3_Hotbar_ArmsLength",
+						name = "Arms Aerial",
+						uuid = "03737871-6271-be66-8b5b-9f116bf3b30a",
+						variableTogglesType = 2,
+						version = 2.1,
+					},
+				},
+				
+				{
+					data = 
+					{
+						aType = "ACR",
+						conditions = 
+						{
+							
+							{
+								"217ed8f9-6d6d-d200-9e59-ea5bbdc20819",
+								true,
+							},
+							
+							{
+								"ed3bc3cd-1060-9002-bf69-db0f334c79d5",
+								true,
+							},
+							
+							{
+								"9c1a1301-1d97-0861-b6ab-464748ee0f71",
+								true,
+							},
+							
+							{
+								"56dd8ef9-661f-7df1-8ceb-77bfd4305f24",
+								true,
+							},
+						},
+						gVar = "ACR_RikuRDM3_Hotbar_Surecast",
+						name = "SureC Aerial",
+						uuid = "73975039-e26a-a4a8-82b5-31ce5ee85a08",
+						variableTogglesType = 2,
+						version = 2.1,
+					},
+				},
+			},
+			conditions = 
+			{
+				
+				{
+					data = 
+					{
+						category = "Self",
+						conditionType = 8,
+						localmapid = 1372,
+						uuid = "217ed8f9-6d6d-d200-9e59-ea5bbdc20819",
+						version = 3,
+					},
+					inheritedIndex = 1,
+				},
+				
+				{
+					data = 
+					{
+						category = "Event",
+						eventArgOptionType = 2,
+						eventEntityContentID = 5640,
+						name = "Shinryu",
+						uuid = "ed3bc3cd-1060-9002-bf69-db0f334c79d5",
+						version = 3,
+					},
+				},
+				
+				{
+					data = 
+					{
+						category = "Event",
+						eventArgType = 2,
+						eventSpellID = 50261,
+						name = "Tidal Wave",
+						uuid = "c801f14c-2655-bdf2-9e27-d57c366eec5f",
+						version = 3,
+					},
+				},
+				
+				{
+					data = 
+					{
+						category = "Event",
+						eventArgType = 2,
+						eventSpellID = 50235,
+						name = "Aerial Blast",
+						uuid = "9c1a1301-1d97-0861-b6ab-464748ee0f71",
+						version = 3,
+					},
+					inheritedIndex = 4,
+				},
+				
+				{
+					data = 
+					{
+						category = "Party",
+						channelCheckTimeRemain = 6,
+						channelCheckType = 3,
+						comparator = 2,
+						conditionType = 5,
+						name = "6s or less",
+						partyTargetType = "Event Owner",
+						uuid = "56dd8ef9-661f-7df1-8ceb-77bfd4305f24",
+						version = 3,
+					},
+				},
+			},
+			enabled = false,
+			eventType = 3,
+			name = "Shinryu KB",
+			uuid = "dc82ce96-32dc-707a-aab4-b2ca310ac369",
+			version = 2,
+		},
+	}, 
+	inheritedProfiles = 
+	{
+	},
+}
+
+
+
+return tbl
